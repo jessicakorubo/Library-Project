b85e0fe700d34c30a81b25bc85a0228690d50ce6
Jessica <jessicakorubo@gmail.com>
2023-08-13 07:21:22 +0100

    Fiished borrowe html page

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const inactive_btn = document.querySelectorAll('button.inactive');
+const table_btn = document.querySelectorAll('button.table-btn');
 
-var inactive_button = [].slice.call(inactive_btn);
+var my_btn = [].slice.call(table_btn);
 
 
 
-console.log('this is', inactive_btn,'inactive');
-inactive_button.forEach(btn => {
-    btn.classList.add('inactive');
-    btn.innerHTML ='EXHAUSTED';
+console.log('this is', my_btn,'inactive');
+my_btn.forEach(btn => {
+
+    if (btn.classList.contains('pending')) {
+        btn.innerHTML = 'PENDING...'
+    }
+    else if (btn.classList.contains('returned')) {
+        btn.innerHTML ='RETURNED';
+    }
+    
 });
