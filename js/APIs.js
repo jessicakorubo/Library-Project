3461460fc0d5413a902d92f5a2e579f882af15f3
Noah-V <noahvikoo3@gmail.com>
2023-08-16 14:23:45 +0100

    Noah's Commit

--- js/APIs.js
+++ js/APIs.js
@@ -3,9 +3,48 @@ $(".cancelbtn").click((event) => {
   window.location.replace("../index.html");
 });
 
+function fetchData(url, method, token) {
+  fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + token,
+    },
+    // body,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => { 
+      console.log(data);
+      // return data
+    })
+    .catch((error) => error);
+}
+
+
 const form = $("#login-form");
 
 let token = "";
+
+function fetchData(url, method, token) {
+  fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + token,
+    },
+  })
+    .then(response => { 
+      return response.json()
+    })
+    .then(data => { 
+      console.log(data)
+      // return data
+    })
+    .catch((error) => error);
+  
+}
 
 function login() {
   const username = $("#username").val();
@@ -85,6 +124,58 @@ $("#close").click((e) => {
   $("#username").val("").focus();
 });
 
+function viewBooks() {
+  fetch('http://localhost:8080/api/v1/books', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": getAuthorizationValue(),
+      }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+    data.forEach(data => {
+      var bodyOfBooksTable = document.getElementById('view-books-table-body');
+      bodyOfBooksTable.innerHTML = 
+        ` <tr>
+            <td>${data.isbn}</td>
+            <td>${data.title}</td>
+            <td>${data.author}</td>
+            <td>Rosamunde Piilcher</td>
+            <td>Novel</td>
+            <td>3</td>
+
+            <td>
+              <button type="button" class="details">DETAILS</button>
+            </td>
+            <td>
+              <div class="button-div">
+                <button type="button" class="delete">DELETE</button>
+                <div class="popup">
+                  <div class="qtn">
+                    Are you sure you want to delete this book?
+                  </div>
+                  <div class="delete-options">
+                    <a class="cancel" href="">Cancel</a>
+                    <a class="delete" href="#">Delete</a>
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+        `
+    });
+    })
+    .catch((error) => error);
+}
+
+var viewBook = $('.view-book-button');
+viewBook.click(
+  viewBooks()
+);
+
 // form.onsubmit = (e) => {
 //   e.preventDefault();
 //   const formData = new FormData(form);
@@ -130,9 +221,8 @@ $("#close").click((e) => {
 //   .catch((error) => console.log(error));
 
 function getAuthorizationValue() {
-  //   const token = getCookie("token");
-  const token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5MjE5MDk5MiwiZXhwIjoxNjkyMTkyNDMyfQ.etk9_14HrJso9EGwCSWFNKpBsKJpIPviuyWU7ToS4HM";
+  // const token = getCookie("");
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5MjE5MDk5MiwiZXhwIjoxNjkyMTkyNDMyfQ.etk9_14HrJso9EGwCSWFNKpBsKJpIPviuyWU7ToS4HM'
   if (token == null) return null;
   return "Bearer " + token;
 }
